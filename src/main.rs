884146b5e7ac35b727c89ff14815135648c78062
Sumeet Rana <sumeetsinh28@gmail.com>
2026-02-27 23:41:15 +0400

    feat: for loop, arrays and mutable bindings

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,25 @@ struct Deck {
 }
 
 fn main() {
-    let deck = Deck { cards: vec![] };
+    // List of 'suits' - 'Hearts', 'Diamonds', 'Clubs', 'Spades'
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
 
-    println!("Heres your deck: {:?}", deck.cards);
+    // List of 'values' - '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+    let values = [
+        "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
+    ];
+
+    let mut cards = vec![];
+
+    // Double nested for loop to create a deck of cards
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+
+    println!("Heres your deck: {:#?}", deck.cards);
 }
