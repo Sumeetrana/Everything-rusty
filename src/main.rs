8bb7afcb812ded0ddbe8eb4a83d67ab80e76e1aa
Sumeet Rana <sumeetsinh28@gmail.com>
2026-02-28 23:45:05 +0400

    feat: implementations and associated function(new)

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,33 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // List of 'suits' - 'Hearts', 'Diamonds', 'Clubs', 'Spades'
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+impl Deck {
+    fn new() -> Self {
+        // List of 'suits' - 'Hearts', 'Diamonds', 'Clubs', 'Spades'
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
 
-    // List of 'values' - '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
-    let values = [
-        "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
-    ];
+        // List of 'values' - '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+        let values = [
+            "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
+        ];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    // Double nested for loop to create a deck of cards
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        // Double nested for loop to create a deck of cards
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        let deck = Deck { cards };
+        return deck;
     }
+}
 
-    let deck = Deck { cards };
-
+fn main() {
+    let deck = Deck::new();
     println!("Heres your deck: {:#?}", deck.cards);
 }
+ 
