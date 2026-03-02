cb7b8099776e3642af245a6821b4a687213fa8d1
Sumeet Rana <sumeetsinh28@gmail.com>
2026-03-02 23:52:32 +0400

    feat: created Bank and account struct and implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
 }
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
+fn main() {
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("John"));
+
+    print_account(account);
+}
+ 
