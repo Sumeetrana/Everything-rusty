6fac591c5a38fef436ec5b1bf7675c556b38537b
Sumeet Rana <sumeetsinh28@gmail.com>
2026-03-10 12:44:32 +0530

    feat: extracting errors from the file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::fs;
 
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 fn main() {
     let text = fs::read_to_string("logs.txt");
 
     match text {
         Ok(value) => {
-            println!("{}", value);
+            let error_logs = extract_errors(value.as_str());
+            println!("{:#?}", error_logs);
         }
         Err(error) => {
             println!("{}", error);
