d945102dd4e53a1ec2975be045dfa0485f9fb7b0
Sumeet Rana <sumeetsinh28@gmail.com>
2026-03-10 20:21:59 +0530

    feat: code refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,24 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
+    // let text = fs::read_to_string("logs.txt");
 
-    match text {
-        Ok(value) => {
-            let error_logs = extract_errors(value.as_str());
-            println!("{:#?}", error_logs);
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(()) => println!("Error logs success"),
-                Err(error) => println!("{:#?}", error),
-            }
-        }
-        Err(error) => {
-            println!("{}", error);
-        }
-    }
+    // match text {
+    //     Ok(value) => {
+    //         let error_logs = extract_errors(value.as_str());
+    //         println!("{:#?}", error_logs);
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(()) => println!("Error logs success"),
+    //             Err(error) => println!("{:#?}", error),
+    //         }
+    //     }
+    //     Err(error) => {
+    //         println!("{}", error);
+    //     }
+    // }
+
+    let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
 }
