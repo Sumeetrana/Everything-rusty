dc54acbd029147eccd7cd34c58a6a23650fb72d5
Sumeet Rana <sumeetsinh28@gmail.com>
2026-03-15 14:22:31 +0530

    feat: shorten strings using mutable iter and truncate

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,18 @@ fn print_elements(elements: &[String]) {
         .for_each(|el| println!("{}", el));
 }
 
+fn shorten_string(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
 fn main() {
-    let colors = vec![
+    let mut colors = vec![
         String::from("red"),
         String::from("blue"),
         String::from("green"),
     ];
 
-    print_elements(&colors);
+    // print_elements(&colors);
+    shorten_string(&mut colors);
+    println!("{:#?}", colors);
 }
