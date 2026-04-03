29dca01d62261dd03bc46c6d5a0e173a32d8dd3b
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-03 14:34:21 +0530

    feat: implementation complete

--- src/implementation.rs
+++ src/implementation.rs
@@ -8,14 +8,22 @@ impl Rectangle {
     fn area(&self) -> u16 {
         self.breadth * self.length
     }
+
+    fn update_len(&mut self, new_len: u16) {
+        self.length = new_len;
+    }
 }
 
 fn main() {
-    let rec = Rectangle {
+    let mut rec = Rectangle {
         length: 20,
         breadth: 20,
     };
 
     println!("Area: {}", rec.area());
     println!("Area2: {}", rec.area());
+
+    rec.update_len(30);
+
+    println!("Updated area: {}", rec.area());
 }
