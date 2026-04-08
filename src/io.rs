99dda92b235e67a96841e37d2354826c0295e8bb
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-08 23:48:59 +0530

    feat: removed io

--- src/io.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-fn main() {
-    let mut input = String::new();
-    println!("Please input your name:");
-    io::stdin().read_line(&mut input).expect("Input failed");
-    println!("User input: {}", input);
-}
