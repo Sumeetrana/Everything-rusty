908a364e2e8661a9bf2bf29f0450042d84f5fe73
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-17 23:59:04 +0530

    feat: thread error condition withour scoped thread

--- src/scope_threads.rs
+++ src/scope_threads.rs
@@ -1,9 +1,20 @@
+struct Person {
+    first_name: String,
+}
+
 pub fn test_scoped_threads() {
     let age = 34;
+    let person01 = Person {
+        first_name: String::from("Trevor"),
+    };
 
     let print_age = move || {
         println!("Your age is {age}");
+        println!("your name is {}", person01.first_name);
     };
+
+    println!("Your age is {age}");
+    println!("your name is {}", person01.first_name); // Error
 
     let _result = std::thread::spawn(print_age).join();
 
