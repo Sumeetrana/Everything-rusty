54d4d87ec5a234449a3f0d8876ed189858559ee9
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-18 00:03:49 +0530

    feat: scoped spawn thread

--- src/scope_threads.rs
+++ src/scope_threads.rs
@@ -8,15 +8,21 @@ pub fn test_scoped_threads() {
         first_name: String::from("Trevor"),
     };
 
-    let print_age = move || {
+    let print_age = || {
         println!("Your age is {age}");
-        println!("your name is {}", person01.first_name);
+        println!("your name is {}", &person01.first_name);
     };
 
-    println!("Your age is {age}");
-    println!("your name is {}", person01.first_name); // Error
+    // println!("Your age is {age}");
+    // println!("your name is {}", person01.first_name); // Error
 
-    let _result = std::thread::spawn(print_age).join();
+    // let _result = std::thread::spawn(print_age).join();
+    std::thread::scope(|scope| {
+        scope.spawn(print_age);
+    });
+
+    println!("Your age is {age}");
+    println!("your name is {}", person01.first_name);
 
     println!("Finished printing age");
 }
