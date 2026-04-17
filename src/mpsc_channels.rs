a0949fe7e0b234431a97c22c65f16094b18d38b7
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-18 00:42:40 +0530

    feat: receive message that are sent from mpsc_channels

--- src/mpsc_channels.rs
+++ src/mpsc_channels.rs
@@ -1,4 +1,7 @@
-use std::sync::mpsc;
+use std::{
+    sync::mpsc::{self, Receiver},
+    time::Duration,
+};
 
 pub fn test_mpsc_channels() {
     let (transmitter, reciever) = mpsc::channel::<u8>();
@@ -7,4 +10,12 @@ pub fn test_mpsc_channels() {
     println!("Send status: {}", send_result.is_ok());
 
     transmitter.send(152);
+
+    let receive_result = reciever.recv_timeout(Duration::from_millis(300));
+    println!("Receive result is: {}", receive_result.is_ok());
+    println!("Receive result is: {}", receive_result.unwrap());
+
+    let receive_result = reciever.recv_timeout(Duration::from_millis(300));
+    println!("Receive result is: {}", receive_result.is_ok());
+    println!("Receive result is: {}", receive_result.unwrap());
 }
